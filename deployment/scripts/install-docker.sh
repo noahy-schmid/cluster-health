9e1b5900021b2e224e187f3da0ccc29c10566421
Noah- Yannick Schmid <44346907+nonoyona@users.noreply.github.com>
2025-09-05 23:45:27 +0200

    aim to fix docker installation

--- deployment/scripts/install-docker.sh
+++ deployment/scripts/install-docker.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
 # Add Docker's official GPG key
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --batch --yes -o /etc/apt/keyrings/docker.gpg
 
 # Add Docker repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
